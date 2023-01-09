6265618158e9fcd52d51da94de077c95e5255a03
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 15:20:03 +0530

    Modified dropdown

--- front-end/src/components/DropDown.js
+++ front-end/src/components/DropDown.js
@@ -8,7 +8,9 @@ const DropDown = (props) => {
                 {label}
             </label>
             <div className="relative">
-                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 
+                        text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white
+                        focus:border-gray-500"
                     id="grid-state"
                     value={value}
                     onChange={(e) => onChange(e.target.value)}>
