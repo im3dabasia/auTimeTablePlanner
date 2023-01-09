582775fb56a7f2a3d1fc836de2c24f9fe37b656b
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 13:32:25 +0530

    Updated db.js with comments

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 require("dotenv").config() 
 
+// process.env.MONGO_URL is a variable that connects the appliaction to the database hosted Online: Mongodb Atlas Clusters
 const connectToDataBase = () => {
     mongoose
         .connect(process.env.MONGO_URL, {
