accb698ce36dbbadc33c146198bcb1361a7740ed
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 15:19:15 +0530

    Modified button

--- front-end/src/components/Button.js
+++ front-end/src/components/Button.js
@@ -4,7 +4,8 @@ const Button = (props) => {
     const {onClick, label} = props;
 
     return (
-        <button className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 mx-2 border-b-4 border-red-700 hover:border-red-500 rounded"  
+        <button className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 
+                    px-4 mx-2 border-b-4 border-red-700 hover:border-red-500 rounded"  
         onClick={onClick} >
             {label}
         </button>
