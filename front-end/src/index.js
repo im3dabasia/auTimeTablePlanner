3af9297aaa54e1965c8e4fe890ea28b03b992bcc
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 15:47:14 +0530

    Modified index.js to preetify the base code

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";    
+import App from "./App";
 import Navbar from "./components/NavBar";
 import 'tw-elements';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
-        <Navbar/>
+        <Navbar />
         <App />
     </Router>
-    );
+);
 
-    
