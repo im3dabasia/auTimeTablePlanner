1978243945fd15cef94464e5f31d1affb2797fa3
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 15:46:40 +0530

    Preetified App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,18 @@
-
+// External Modules
 import { Route, Routes } from "react-router-dom";
+
+// Local Modules
 import LandingPage from './pages/LandingPage.js';
 import Register from "./pages/auth/Register.js";
 import Login from "./pages/auth/Login.js"
 import Courses from "./pages/Courses.js";
-import './index.css'
 import DashBoard from "./pages/DashBoard.js";
+
+// CSS files
+import './index.css'
+
 function App() {
   return (
-    
     <Routes>
         <Route path="/" element={<LandingPage/>} />
         <Route path="/register" element={<Register />} />
