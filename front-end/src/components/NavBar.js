f6d35ffa920070f849ed241b00bb92635b09a939
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 15:39:18 +0530

    Preetiefied Navbar

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -62,13 +62,13 @@ export default function Navbar() {
             </div>) : null
         }
 
-        <button data-collapse-toggle="navbar-default" type="button" 
+        <button data-collapse-toggle="navbar-default" type="button"
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden
             hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400
             dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" 
+            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
               clip-rule="evenodd">
             </path>
           </svg>
@@ -76,22 +76,28 @@ export default function Navbar() {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           {
             localStorage.getItem('STTP-user') ? (
-              < ul className="flex flex-row p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 
-                md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              < ul
+                className="flex flex-row p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50
+                   md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white
+                 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+              >
                 <div>
                   <Link to="/dashboard" >
                     <Button className="bg-red-500 hover:bg-red-400 text-white 
-                      font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded" label="Time Table" >
+                                font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
+                      label="Time Table">
                     </Button>
                   </Link>
                   <Link to="/courseselection" >
                     <Button className="bg-red-500 hover:bg-red-400 text-white 
-                      font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded" label="Course Selection" >
+                                font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded" 
+                      label="Course Selection" >
                     </Button>
                   </Link>
                   <Link to="/" >
                     <Button className="bg-red-500 hover:bg-red-400 text-white 
-                      font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded" label="Log out" onClick={logOut}>
+                                font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded" 
+                      label="Log out" onClick={logOut}>
                     </Button>
                   </Link>
                 </div>
