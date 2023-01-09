8ef865a49b21748d4aadfa6e2c6c1cbf444199e3
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 15:48:24 +0530

    Commented modules in index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,9 +1,14 @@
+// External Modules
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+
+// Local Modules
 import App from "./App";
 import Navbar from "./components/NavBar";
+
+// CSS and Other elements
+import "./index.css";
 import 'tw-elements';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
