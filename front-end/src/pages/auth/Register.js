37ae54804ad61a05797638d7e17a56cdf871e965
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 15:40:27 +0530

    Preetified Register

--- front-end/src/pages/auth/Register.js
+++ front-end/src/pages/auth/Register.js
@@ -84,9 +84,9 @@ const Register = () => {
             <input
               type="number"
               className="form-control block w-full px-3 py-1.5 text-base font-normaltext-gray-700
-                    bg-white bg-clip-padding border border-solid border-gray-300 rounded
-                      transition ease-in-out m-0 focus:text-gray-700 focus:bg-white
-                    focus:bord`er-blue-600 focus:outline-none"
+                bg-white bg-clip-padding border border-solid border-gray-300 rounded
+                  transition ease-in-out m-0 focus:text-gray-700 focus:bg-white
+                  focus:bord`er-blue-600 focus:outline-none"
               id="rollNum"
               placeholder="Input your roll number here"
             />
@@ -96,9 +96,9 @@ const Register = () => {
             <input
               type="string"
               className="form-control block w-full px-3 py-1.5 text-base font-normaltext-gray-700
-                    bg-white bg-clip-padding border border-solid border-gray-300 rounded 
-                      transition ease-in-out m-0 focus:text-gray-700 focus:bg-white
-                    focus:border-blue-600 focus:outline-none"
+                bg-white bg-clip-padding border border-solid border-gray-300 rounded 
+                  transition ease-in-out m-0 focus:text-gray-700 focus:bg-white
+                focus:border-blue-600 focus:outline-none"
               id="name"
               placeholder="Input your name here"
             />
@@ -108,9 +108,9 @@ const Register = () => {
             <input
               type="password"
               className="form-control block w-full px-3 py-1.5 text-base font-normaltext-gray-700
-                   bg-white bg-clip-padding border border-solid border-gray-300 rounded transition 
-                     ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 
-                     focus:outline-none"
+                bg-white bg-clip-padding border border-solid border-gray-300 rounded transition 
+                  ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 
+                  focus:outline-none"
               id="passWord"
               placeholder="Password input"
             />
