aeb7f6e67d80701d6a6aa458c4f43a249a2d9722
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 13:36:04 +0530

    Commented calendarController with imp comments

--- back-end/controllers/calendarController.js
+++ back-end/controllers/calendarController.js
@@ -1,21 +1,22 @@
 const CourseModel = require("../models/course.js");
 
+// Global scope to store user Roll number once logged in
 let loggedInRollNumber = 0;
+
+// Send calendar information based on the student roll number.
 const studentCalendarInfo = async (req, res) => {
     let data = await CourseModel.find({studentRollNum : loggedInRollNumber})
     if(data != null ){
       return res.status(200).json(data)
-
     }
-
     return res.status(200).json({"NO DATA":"NO DATA"})
 }
 
+// Send Roll number information based.
 const sendRollNumber = async (req, res) => {
     const data = await req.body
     loggedInRollNumber = data.rollNum
     return res.status(200).json(data)
-
 }
 
 module.exports = {studentCalendarInfo, sendRollNumber };
