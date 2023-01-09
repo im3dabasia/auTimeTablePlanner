c53a0b3f9ab4371288009dda0ddc93e831cf716b
im3dabasia <eshaandabasiya@gmail.com>
2023-01-09 13:58:23 +0530

    course.js deleted extra lines

--- back-end/models/course.js
+++ back-end/models/course.js
@@ -5,47 +5,39 @@ const CourseSchema = new mongoose.Schema({
     type:Number,
     required: true,
     unique:false
-
   },
   courseName:{
     type:String,
     required: true,
     unique:false
-
   },
   courseStartTime:{
     type:String,
     required:true,
-
   },
   courseEndTime:{
     type:String,
     required:true,
-
   },
   courseWeeklyFirstLec:{
     type:String,
     required:true,
     min:0,
     max:4
-
   },
   courseWeeklySecondLec:{
     type:String,
     required:true,
     min:0,
     max:4
-
   },
   courseDescription:{
     type:String,
     unique:false
-
   },
   courseFaculty:{
     type:String,
     required:false
-
   }
 });
 
